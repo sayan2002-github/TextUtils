8255de0ea81867fc2234b2cd762f59316ff3c61e
Sayan <sayanparui076@gmail.com>
2022-09-20 08:45:52 +0530

    Updates

---
+++ precache-manifest.e5b0f5b82b716dc8b0bb1edce57df58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da57e93a90800f45f220",
+    "url": "/TextUtils/static/css/main.2c801847.chunk.css"
+  },
+  {
+    "revision": "da57e93a90800f45f220",
+    "url": "/TextUtils/static/js/main.da57e93a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "c2e9ea1094656355ab4077033302da95",
+    "url": "/TextUtils/index.html"
+  }
+];
